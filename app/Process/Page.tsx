4ec6ba34b245249f1c5bdd14a871482672456af5
Ashok Reddy <ashok17748@gmail.com>
2024-01-26 14:34:19 +0530

    fewChangess

--- app/Process/Page.tsx
+++ app/Process/Page.tsx
@@ -1,30 +1,25 @@
-import React from "react";
-import Formelement from "../Components/Formelement";
-import Navbar from "../Components/Navbar";
-import TabsDesign from "../Components/TabsDesign";
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "SaaS checklist | Process",
-  description: "SaaS, SaaS checkklist ,Customer Acquision",
-};
-
+import React from 'react'
+import Formelement from '../Components/Formelement';
+import Navbar from '../Components/Navbar';
+import TabsDesign from '../Components/TabsDesign';
 function Page() {
   return (
-    <main>
-      <Navbar />
+    <div>
+      {" "}
+      <main>
+        <Navbar />
 
-      <TabsDesign
-        h1="Process🚶🏻"
-        h3="A easy user journey, from awareness to account deletion, strengthens customer loyalty. Comprehensive support, effective onboarding, and strategies for renewal and retention contribute to a positive customer experience."
-      />
+        <TabsDesign
+          h1="Process🚶🏻"
+          h3="A easy user journey, from awareness to account deletion, strengthens customer loyalty. Comprehensive support, effective onboarding, and strategies for renewal and retention contribute to a positive customer experience."
+        />
 
-      <div className="flex flex-col gap-4 ">
-        <Formelement tab="Process" />
-      </div>
-    </main>
+        <div className="flex flex-col gap-4 ">
+          <Formelement tab="Process" />
+        </div>
+      </main>
+    </div>
   );
 }
-export const dynamic = "force-dynamic";
 
-export default Page;
+export default Page
